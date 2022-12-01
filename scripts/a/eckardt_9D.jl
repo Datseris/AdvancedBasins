5b191a9372f56aa4cea31c42b62537cd7c8d1d69
awage <>
2022-12-01 22:01:21 +0100

    add bif function

--- scripts/a/eckardt_9D.jl
+++ scripts/a/eckardt_9D.jl
@@ -62,8 +62,7 @@ function E9D!(du, u, p, t)
     du[9] = -u[9]*k[9]/Re + σ[31]*u[2]*u[3] + σ[32]*u[6]*u[8] 
 end
 
-function continuation_E9D()
-    Re_range = range(300,450, length = 20)
+function continuation_E9D(Re_range)
     p = E9DParameters(; Re = 337.)
     ds = ContinuousDynamicalSystem(E9D!, zeros(9), p, (J,z0, p, n) -> nothing)
     diffeq = (alg = Vern9(), reltol = 1e-9, maxiters = 1e8)
@@ -73,11 +72,11 @@ function continuation_E9D()
         mx_chk_fnd_att = 1000, stop_at_Δt = true, store_once_per_cell = true,
         mx_chk_loc_att = 1000, mx_chk_safety = Int(1e7), show_progress = true,
         mx_chk_att = 10, diffeq)
-    pidx = :Re; spp = 5000
+    pidx = :Re; spp = 10000
     sampler, = Attractors.statespace_sampler(Random.MersenneTwister(1234); min_bounds = ones(9).*(-1.), max_bounds = ones(9).*(1.))
 
     ## RECURENCE CONTINUATION
-    continuation = RecurrencesSeedingContinuation(mapper; threshold = 0.5)
+    continuation = RecurrencesSeedingContinuation(mapper; threshold = Inf)
     fs, att = basins_fractions_continuation(
             continuation, Re_range, pidx, sampler;
             show_progress = true, samples_per_parameter = spp
@@ -147,7 +146,47 @@ function plot_filled_curves(fractions, prms, figurename)
 # Makie.save("lorenz84_fracs.png", fig; px_per_unit = 4)
 end
 
-f,a,r = continuation_E9D()
+function plot_bif(arange, att)
+    a = arange; 
+    diffeq = (alg = Vern9(), reltol = 1e-9, maxiters = 1e8)
+    s = Vector{Int32}[]
+    for e in att 
+        push!(s, collect(keys(e)))
+    end
+    s = unique(vcat(s...))
+    ptlst = Vector{Vector{Vector{Float64}}}(undef,length(s))
+    for k in 1:length(s); ptlst[k] = []; end
+    for (k,el) in enumerate(att)
+        p = E9DParameters(; Re = a[k])
+        ds = ContinuousDynamicalSystem(E9D!, zeros(9), p, (J,z0, p, n) -> nothing)
+        @show el
+        if el ≠ 0 
+            for p in el
+                # set_parameter!(df, [a[k], ν])
+                tra = trajectory(ds, 100, p[2][1]; Ttr = 200, diffeq)
+                for y in tra
+                    v = [a[k], y[1]]
+                    push!(ptlst[p[1]], v)
+                end
+            end
+        end
+    end
+    fig = Figure(resolution = (1300, 900))
+    ax = Axis(fig[1,1], ylabel = "xn", yticklabelsize = 20, xticklabelsize = 20, ylabelsize = 20)
+    for (j,p) in enumerate(ptlst)
+        P = Dataset(p)
+        scatter!(ax, P[:,1],P[:,2], markersize = 0.7, color = Cycled(j), rasterize = 4)
+    end
+    save("diag_bif_e9d.png",fig)
+    return ptlst 
+end
+
+
+Re_range = range(300,450, length = 50)
+f,a,r = continuation_E9D(Re_range)
 save("eckhardt_cont_full.jld2","f",f,"a",a,"r",r)
-# @load "eckhardt_cont_projected.jld2"
+# # @load "eckhardt_cont_projected.jld2"
 plot_filled_curves(f,r,"continuation_eckhardt_9D_full.png")
+
+# ptlst = plot_bif(Re_range ,a)
+
